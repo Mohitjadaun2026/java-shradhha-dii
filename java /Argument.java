00f319f6e28ffbf50a637c4155e46d1e3a2e7a4a
Mohitjadaun2026 <126583966+Mohitjadaun2026@users.noreply.github.com>
2024-06-01 10:42:34 +0000

    array

---
+++ java /Argument.java
@@ -0,0 +1,20 @@
+
+/// array as argument 
+
+public class Argument{
+    public static void update(int marks[]) {
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = marks[i] + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int marks[] = {65, 78, 96};
+        update(marks);
+
+        for (int i = 0; i < marks.length; i++) {
+            System.out.print(marks[i] + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ java /Array.java
@@ -0,0 +1,9 @@
+//  public class Array{
+//     public static void main(String[] args) {
+//         int marks[]= new int[59];
+//         int numbers[] ={3,4,5};
+//         String fruits[]={"apple", "mango", "banana"};
+//     }
+//  }
+
+
